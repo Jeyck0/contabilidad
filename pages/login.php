86f3aef4680e85c9f1bf4e6e053d3f7c2d384311
Jeyck0 <obrequel337@gmail.com>
2019-04-14 15:40:41 -0400

    agregado interfaz login

--- pages/login.php
+++ pages/login.php
@@ -12,7 +12,7 @@
   <title>SB Admin 2 - Login</title>
 
   <!-- Custom fonts for this template-->
-  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendors/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -41,14 +41,21 @@
                   </div>
                   <form class="user" method="POST" action="../procesos/login.php">
                     <div class="form-group">
-                      <input type="text" name="rut_in" class="form-control form-control-user" id="rut_in"  placeholder="Ingrese Rut EJ : 12345678-0" required>
+                      <input type="text" name="rut_in" class="form-control " id="rut_in"  placeholder="Usuario" required>
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="rut_in" class="form-control " id="rut_in"  placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                      <select class="form-control  " required>
+                        <option selected disabled >--Seleccionar Colegio--</option>
+                        <option>Maria Auxiliadora</option>
+                        <option>Laura Vicuña</option>
+                      </select>
                     </div>
                     <button type="submit" id="btn_login" class="btn btn-primary btn-user btn-block" name="btn_login">
                       Entrar
                     </button>
-                    <div class="form-group mt-3 text-center">
-                      <a href="profesor_login.php">Ingresar como Profesor</a>
-                    </div>
                     <hr>
                   </form>
                   <hr>
@@ -65,11 +72,11 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../vendors/jquery/jquery.min.js"></script>
+  <script src="../vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="../vendors/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="../js/sb-admin-2.min.js"></script>
