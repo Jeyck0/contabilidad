a013a73bba08faadec87f8fc0502916a3da31a50
Jeyck0 <obrequel337@gmail.com>
2019-04-13 21:21:17 -0400

    pruebas con jsonp

--- js/ajaxData.php
+++ js/ajaxData.php
@@ -3,18 +3,27 @@
 require_once ('../clases/Conexion.php');
 header ('Access-Control-Allow-Origin: * ');
 
-$conexion = new Conexion();
-$cnn =$conexion->conectar();
 
 
-$id_padre = $_POST["padre_id"];
+//$id_padre = $_GET["padre_id"];
 
+function arraysql($id){
+    $conexion = new Conexion();
+    $cnn =$conexion->conectar();
 
-$query ="select id_cat_egreso,nombre,codigo from cat_egresos where id_cat_padre_egresos = '$id_padre' order by codigo ASC";   
-$consultar = mysqli_query($cnn,$query);
+    $query ="select id_cat_egreso,nombre,codigo from cat_egresos where id_cat_padre_egresos = '$id' order by codigo ASC";   
+    $consultar = mysqli_query($cnn,$query);
+    $i = 0;
 
-        while($dado = mysqli_fetch_assoc($consultar)){
-            echo "<option value='".$dado['id_cat_egreso']."' >".$dado['codigo'] ."  | ". $dado['nombre']. " </option>  ";
+        while($dado = mysqli_fetch_array($consultar)){
+            $dadoData[$i]= $dado;
+            $i++;
         }
+
+        return $dadoData;
+}
+
+$jsonArray = arraysql(3);
+echo json_encode($jsonArray);
 
 
--- js/categorias.js
+++ js/categorias.js
@@ -9,25 +9,23 @@ $(function(){
         var padreID = $(this).val();
         var getUrl = window.location;
         var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-        //alert(baseUrl+"/js/ajaxData.php");
-        //esto es test
+
+
+        var callback = function(data){
+            console.log(data);
+        }
+        
         if(padreID){
             $.ajax({
-                url:baseUrl+"/js/ajaxData.php",
-                method:"POST",
+                url:baseUrl+"/js/ajaxData.php?callback=callback",
+                method:"GET",
                 data: 'padre_id='+padreID,
-                error: function(request, errorcode, errortext){
-                    $("#respuesta").html("<p>Ocurrió el siguiente error: <strong>" + errortext + "</strong></p>");
-               }, 
-                success: function(html){
-                    $('#select_cat_egresos').html(html);
-
-                }
+                dataType:"jsonp",
+                jsonpCallback:"jsonCallback"
             }); 
-        }else{
-            alert("malo");
-            $('#select_cat_egresos').html('<option value="">Seleccione cuenta general</option>');
-
         }
+
+
+
     });
 });
